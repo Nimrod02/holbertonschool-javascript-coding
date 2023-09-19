624d3bb4acfbedcf366a64fbd39075093502be93
nimrod02 <letrun.a@gmail.com>
2023-09-19 13:57:58 +0200

    another fixing for the linter

--- javascript-web_scraping/0-readme.js
+++ javascript-web_scraping/0-readme.js
@@ -4,10 +4,10 @@ const fs = require('fs');
 
 const filePath = process.argv[2];
 
-fs.readFile(filePath, 'utf-8', function (err, data) {
-  if (err) {
-    console.log('error: ${err}');
-  } else {
-    console.log(data);
-  }
+fs.readFile(filePath, 'utf-8', function(err, data) {
+    if (err) {
+        console.error(`An error occurred: ${err}`);
+    } else {
+        console.log(data);
+    }
 });

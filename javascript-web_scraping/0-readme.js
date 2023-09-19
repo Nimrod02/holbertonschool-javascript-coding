3396fdc8d2cb0d169aab14793a146ab04823a657
nimrod02 <letrun.a@gmail.com>
2023-09-19 12:31:28 +0200

    fix linter task 0

--- javascript-web_scraping/0-readme.js
+++ javascript-web_scraping/0-readme.js
@@ -4,10 +4,10 @@ const fs = require('fs');
 
 const filePath = process.argv[2];
 
-fs.readFile(filePath, 'utf-8', function(err, data) {
-        if (err) {
-                console.log('error');
-        } else {
-                console.log(data);
-        }
-})
+fs.readFile(filePath, 'utf-8', function (err, data) {
+  if (err) {
+    console.log('error: ${err}');
+  } else {
+    console.log(data);
+  }
+});

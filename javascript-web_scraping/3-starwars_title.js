76c2a0669cc39376f40790836a3522aa45c3ad7a
nimrod02 <letrun.a@gmail.com>
2023-09-20 11:26:18 +0200

    add task 3

---
+++ javascript-web_scraping/3-starwars_title.js
@@ -0,0 +1,15 @@
+#!/usr/local/bin/node
+
+const request = require('request');
+
+const movieId = process.argv[2];
+
+const url = `https://swapi-api.hbtn.io/api/films/${movieId}`;
+
+request.get(url, (error, response, body) => {
+  if (error) {
+    console.error(`Erreur : ${error}`);
+  } else {
+    console.log(JSON.parse(body).title);
+  }
+});

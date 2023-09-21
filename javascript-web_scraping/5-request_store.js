b7b2666802f003aa5c55ae7687b28483bf4e9696
nimrod02 <letrun.a@gmail.com>
2023-09-21 09:39:25 +0200

    fix task line 8

--- javascript-web_scraping/5-request_store.js
+++ javascript-web_scraping/5-request_store.js
@@ -5,7 +5,7 @@ const fs = require('fs');
 
 const filePath = process.argv[3];
 
-request.get(process.argv[2], (error, body, result) => {
+request.get(process.argv[2], (error, response, body) => {
   if (error) {
     console.error(`error: ${error}`);
   } else {

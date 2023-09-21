bc035638e76d8f9fd1b8c6e3ec028d1a071eda99
nimrod02 <letrun.a@gmail.com>
2023-09-21 09:45:29 +0200

    rewrite the task

--- javascript-web_scraping/5-request_store.js
+++ javascript-web_scraping/5-request_store.js
@@ -7,8 +7,12 @@ const filePath = process.argv[3];
 
 request.get(process.argv[2], (error, response, body) => {
   if (error) {
-    console.error(`error: ${error}`);
+    console.error(`Erreur lors de la requête : ${error}`);
   } else {
-    fs.writeFile(filePath, body, 'utf-8');
+    fs.writeFile(filePath, body, 'utf-8', (err) => {
+      if (err) {
+        console.error(`error: ${err}`);
+      }
+    });
   }
 });

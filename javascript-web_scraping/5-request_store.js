bcfa074bd030d0007551c902b5e88c510ed2850a
nimrod02 <letrun.a@gmail.com>
2023-09-21 09:36:36 +0200

    add task 5

---
+++ javascript-web_scraping/5-request_store.js
@@ -0,0 +1,18 @@
+const request = require('request');
+const fs = require('fs');
+
+const filePath = process.argv[3];
+
+request.get(process.argv[2], (error, body, result) => {
+  if (error) {
+    console.error(`error: ${error}`);
+  } else {
+    fs.writeFile(filePath, body, 'utf-8', (err) => {
+      if (err) {
+        console.error(`error: ${error}`);
+      } else {
+        console.log(filePath);
+      }
+    });
+  }
+});

1ec123fc71f6541850c9218019671627b9e2ae16
Nimrod02 <86027619+Nimrod02@users.noreply.github.com>
2023-09-22 15:14:04 +0200

    Update 0-readme.js

--- javascript-web_scraping/0-readme.js
+++ javascript-web_scraping/0-readme.js
@@ -2,9 +2,7 @@
 
 const fs = require('fs');
 
-const filePath = process.argv[2];
-
-fs.readFile(filePath, 'utf-8', function (err, data) {
+fs.readFile(process.argv[2], 'utf-8', (err, data) => {
   if (err) {
     console.error(`error: ${err}`);
   } else {

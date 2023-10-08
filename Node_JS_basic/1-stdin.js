983db5e056532ea16bfa113799048308efd4f409
nimrod02 <letrun.a@gmail.com>
2023-10-08 12:48:28 +0200

    fix eslint

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,13 +1,13 @@
 const readline = require('readline').createInterface({
   input: process.stdin,
-  output: process.stdout
+  output: process.stdout,
 });
 
 readline.question('Welcome to Holberton School, what is your name?\n', (input) => {
   console.log(`Your name is: ${input}`);
 
   readline.on('close', () => {
-    console.log('This important software is now closing');
+    console.log('This important software is now closing\n');
     readline.close();
   });
 });

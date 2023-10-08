a500aa3706380b7f45c61e911468d26e3b0c590d
nimrod02 <letrun.a@gmail.com>
2023-10-08 12:45:20 +0200

    fix task 1 and stdin

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -5,6 +5,9 @@ const readline = require('readline').createInterface({
 
 readline.question('Welcome to Holberton School, what is your name?\n', (input) => {
   console.log(`Your name is: ${input}`);
-  console.log('This important software is now closing\n');
-  readline.close();
+
+  readline.on('close', () => {
+    console.log('This important software is now closing');
+    readline.close();
+  });
 });
